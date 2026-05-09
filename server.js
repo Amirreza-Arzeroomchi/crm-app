17aa6198bf3015c0882020e1d81d673d1be6a2fa
Amir <arzeroomchi@gmail.com>
2026-05-09 10:06:20 -0700

    fixed uploads route

--- server.js
+++ server.js
@@ -36,6 +36,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
+app.use("/uploads", express.static("public/uploads"));
 
 app.use(
   "/uploads",
